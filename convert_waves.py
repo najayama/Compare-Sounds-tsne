3a305accf36f51ff36eeb683c7b0e94fc3c63090
najayama <27891506+najayama@users.noreply.github.com>
2018-03-01 05:26:00 +0900

    update convert_wave.py

--- convert_waves.py
+++ convert_waves.py
@@ -7,11 +7,32 @@ root.withdraw()
 fTyp = [("","*")]
 iDir = os.path.abspath(os.path.dirname(__file__))
 
-dir ="" 
-while not dir:
+out_dir ="" 
+while not out_dir:
     tkinter.messagebox.showinfo('出力先を選択','変換後のデータを格納するディレクトリを選択してください\n\
 ！注意！\n違う長さのデータは比較できないので、ディレクトリ名にデータの長さを入れておくといいかも。')
-    dir = tkinter.filedialog.askdirectory(initialdir = iDir)
+    out_dir = tkinter.filedialog.askdirectory(initialdir = iDir)
 
-print("出力先を変えたいならプログラムを起動し直してください。\n")
+#切り取る長さを入力するプロンプトを表示
+cut_time = ""
+while cut_time == "":
+    try:
+        cut_time = float(input("切り取るデータの長さを秒(float型)で指定してください。: "))
+    except:
+        cut_time = ""
+        
+#ファイルとstart地点を入力するプロンプトを表示
+
+prompt1 = "切り取るもとのwavファイルを選んでください。"
+convert_list = []
+while True:
+    
+    
+    
+    
+    
+
+
+
+        
 
