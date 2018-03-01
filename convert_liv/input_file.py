c2e5d73e807f59f8bf6be4b49bf136badc0e9cdc
najayama <27891506+najayama@users.noreply.github.com>
2018-03-01 16:01:27 +0900

    update input_file.py

--- convert_liv/input_file.py
+++ convert_liv/input_file.py
@@ -1,9 +1,27 @@
 import os
 
 class InputFile:
-    def __init__(self, input_path, data, framerate):
+    def __init__(self, input_path, framerate, data, start, length):
         self.inpath = input_path
         self.ofilename = os.path.basename(input_path)[:-4] + ".csv"
         self.framerate = framerate
         self.data = data
+        self.start = float(start)
+        self.length = length
+        
+    def show_params(self):
+        """
+        show parametars
+        """
+        print("filename: {}, start={}".format(
+            os.path.basename(self.inpath)
+            , self.start))
+        
+    def check_convertable(self):
+        data_seq =  self.framerate * len(self.data)
+        if(self.length == data_seq):
+            return True
+        else:
+            return False
+        
         
